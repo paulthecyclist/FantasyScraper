e24fd92106abf1f686e93a145915070311aaa6fa
paul davies <pauldavies129@gmail.com>
2016-09-25 14:13:40 +0100

    Updated at hyperdev.com

--- public/client.js
+++ public/client.js
@@ -3,9 +3,6 @@
 
 // by default, you've got jQuery,
 // add other scripts at the bottom of index.html
-
-$(function() {
-  console.log('hello world :o');
 /*  
   $.get('/dreams', function(dreams) {
     dreams.forEach(function(dream) {
@@ -22,7 +19,7 @@ $(function() {
       $('input').focus();
     });
   });
-*/
+
 
  $('form').submit(function(event) {
     event.preventDefault();
@@ -34,3 +31,29 @@ $(function() {
     });
   });
 });
+*/
+
+$('form').submit(function(event) {
+    event.preventDefault();
+    codes = $('input').val();
+    $.post('/getPlayers?' + $.param({codes: codes}), function(players) {
+    
+      console.log(players)
+      var playerHtml = '';
+      var totalPoints = 0;
+      
+      players.sort(function(a, b) {
+    return parseInt(b.points) - parseInt(a.points);
+      });
+      
+      players.forEach(function (player) {
+        playerHtml = playerHtml + player.code + ' - ' + player.name + ' (' + player.points + ')<br/>'; 
+        totalPoints += parseInt(player.points);
+      });
+    
+      $('#player').html(playerHtml);
+      $('#totalPoints').text(totalPoints);
+      $('input').val('');
+      $('input').focus();
+    });
+  });
